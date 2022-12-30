6faf59b7cfa510e3ef4b0b6b10e6c43f7de1ae26
DESKTOP-BPLVUF3\user <jackk0616@gmail.com>
2022-12-30 15:59:52 +0900

    feat(Categoty)

---
+++ src/main/java/menu/domain/Category.java
@@ -0,0 +1,25 @@
+package menu.domain;
+
+public enum Category {
+    JAPANESE(1, "일식"),
+    KOREAN(2, "한식"),
+    CHINESE(3, "중식"),
+    ASIAN(4, "아시안"),
+    WESTERN(5, "양식"),
+    ;
+    private int index;
+    private String name;
+
+    Category(int index, String name) {
+        this.name = name;
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
